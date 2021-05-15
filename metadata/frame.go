a2816c2e20b8a94dcb6c99c26481ff0b646ccc1f
hc5duke <hc5duke@gmail.com>
2021-05-15 04:10:56 -0400

    ffmpeg frame data parsing

--- metadata/frame.go
+++ metadata/frame.go
@@ -1,8 +1,61 @@
 package metadata
 
+import (
+	"errors"
+	"regexp"
+	"strconv"
+)
+
 type Frame struct {
 	Index      int
 	Pts        int
 	PtsTime    float64
 	SceneScore float64
+}
+
+var (
+	//frame:0    pts:1825    pts_time:1.825
+	r1, _ = regexp.Compile(`frame:(\d+)\s+pts:(\d+)\s+pts_time:([\d.]+)\b`)
+
+	//lavfi.scene_score=0.011568
+	r2, _ = regexp.Compile(`scene_score=([\d.]+)\b`)
+
+	ParseError = errors.New("Unexpected input string")
+)
+
+func NewFrame(s0 string, s1 string) (*Frame, error) {
+	m0 := r1.FindStringSubmatch(s0)
+	m1 := r2.FindStringSubmatch(s1)
+
+	if len(m0) != 4 {
+		return nil, ParseError
+	}
+
+	if len(m1) != 2 {
+		return nil, ParseError
+	}
+
+	f, err := strconv.Atoi(m0[1])
+	if err != nil {
+		return nil, err
+	}
+	p, err := strconv.Atoi(m0[2])
+	if err != nil {
+		return nil, err
+	}
+	pt, err := strconv.ParseFloat(m0[3], 64)
+	if err != nil {
+		return nil, err
+	}
+	ss, err := strconv.ParseFloat(m1[1], 64)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Frame{
+		f,
+		p,
+		pt,
+		ss,
+	}, nil
 }
--- metadata/frame_test.go
+++ metadata/frame_test.go
@@ -1,1 +1,41 @@
 package metadata
+
+import (
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"strings"
+	"testing"
+)
+
+func TestNewFrame(t *testing.T) {
+	dat, err := ioutil.ReadFile("../fixtures/time.txt")
+	assert.Nil(t, err)
+
+	strs := strings.Split(string(dat), "\n")
+	f, err := NewFrame(strs[2], strs[3])
+
+	assert.Nil(t, err)
+	assert.Equal(t, 1, f.Index)
+	assert.Equal(t, 1867, f.Pts)
+	assert.Equal(t, 1.867, f.PtsTime)
+	assert.Equal(t, 0.002595, f.SceneScore)
+}
+
+func TestParseError(t *testing.T) {
+	dat, _ := ioutil.ReadFile("../fixtures/time.txt")
+	strs := strings.Split(string(dat), "\n")
+
+	// bad first line
+	_, err := NewFrame("x", strs[1])
+	assert.ErrorIs(t, err, ParseError)
+
+	_, err = NewFrame("frame:0 pts:1825 pts_time:1.8.2.5", strs[1])
+	assert.Error(t, err)
+
+	// bad second line
+	_, err = NewFrame(strs[2], "x")
+	assert.ErrorIs(t, err, ParseError)
+
+	_, err = NewFrame(strs[2], "lavfi.scene_score=0.011.56.8")
+	assert.Error(t, err)
+}
